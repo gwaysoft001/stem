0f603b721143ef2026ff22cd698b5402a9e5a589
gwaysoft001 <gwaysoft@outlook.com>
2019-10-30 09:28:07 +0800

    d

---
+++ swift_playgrounds/swift_playgrounds.mm
@@ -0,0 +1,168 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1572329495688" ID="ID_1794764025" MODIFIED="1572339538518" TEXT="swift">
+<node CREATED="1572329579329" ID="ID_459988178" MODIFIED="1572337173685" POSITION="right" TEXT="swift playgrounds">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.apple.com/swift/playgrounds/">https://www.apple.com/swift/playgrounds/</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1572335727834" ID="ID_1427840658" MODIFIED="1572337164013" TEXT="documents">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://developer.apple.com/documentation/swift_playgrounds">https://developer.apple.com/documentation/swift_playgrounds</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1572337643673" ID="ID_1998264699" MODIFIED="1572337649960" TEXT="lessons">
+<node CREATED="1572337650935" ID="ID_1845453806" MODIFIED="1572337850166" TEXT="learn to code 1 fundamentals of swift">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Conquer levels, puzzles, and coding concepts.
+    </p>
+    <p>
+      Starting with the &#8220;Fundamentals of Swift&#8221; lesson, you&#8217;ll tackle goals using the same code professional developers use every day. As you move along, more advanced concepts come into play. You&#8217;ll continually build on what you&#8217;ve learned and create even more complex code.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1572337888675" ID="ID_239629854" MODIFIED="1572337893776" TEXT="learn to code 2"/>
+<node CREATED="1572337895577" ID="ID_208383898" MODIFIED="1572337903175" TEXT="learn to code 3"/>
+</node>
+</node>
+<node CREATED="1572329683831" ID="ID_1640129690" MODIFIED="1572339460894" POSITION="left" TEXT="office website">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://developer.apple.com/swift/">https://developer.apple.com/swift/ </a>
+    </p>
+    <p>
+      <a href="https://www.apple.com/everyone-can-code/">https://www.apple.com/everyone-can-code/</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1572329700983" ID="ID_1425348020" MODIFIED="1572329780277" TEXT="overview">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://developer.apple.com/swift/">https://developer.apple.com/swift/</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572335600211" ID="ID_1015505727" MODIFIED="1572335613541" TEXT="resources">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://developer.apple.com/swift/resources/">https://developer.apple.com/swift/resources/</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1572337133453" ID="ID_1401701469" MODIFIED="1572337148585" POSITION="right" TEXT="swift.org">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://swift.org/">https://swift.org/</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1572340011366" ID="ID_576102230" MODIFIED="1572340023354" TEXT="swift guide">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html">https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1572339539478" ID="ID_1573808381" MODIFIED="1572339557371" POSITION="left" TEXT="everyone can code">
+<node CREATED="1572339558595" ID="ID_1614563583" MODIFIED="1572342513042" TEXT="Get Started with Code Curriculum Guide">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      guide in baiduyun
+    </p>
+    <p>
+      <a href="https://codespark.com/">https://codespark.com/ 5~9 </a>
+    </p>
+    <p>
+      <a href="https://www.tynker.com/">https://www.tynker.com/ 5~11</a>
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1572339582190" ID="ID_417610144" MODIFIED="1572339684463" TEXT="Swift Playgrounds Curriculum Guide">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      guide in baiduyun
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1572339661087" ID="ID_1691837054" MODIFIED="1572339680743" TEXT="App Development with Swift Curriculum Guide">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      guide in baiduyun
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
