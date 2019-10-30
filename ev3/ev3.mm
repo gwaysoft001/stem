7af138790517a746570bf0c6c4d5ffee5f036b95
gwaysoft001 <gwaysoft@outlook.com>
2019-10-30 09:27:31 +0800

    d

--- ev3/ev3.mm
+++ ev3/ev3.mm
@@ -1,4 +1,366 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1572232759245" ID="ID_420896582" MODIFIED="1572232759245" TEXT="New Mindmap"/>
+<node CREATED="1572232759245" ID="ID_420896582" MODIFIED="1572319886246" TEXT="ev3">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(68, 68, 68)" face="Microsoft Yahei, Hei, Tahoma, SimHei, sans-serif" size="16px"><b>LEGO EV3 45544+45560+2000426</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572234628963" ID="ID_1985625251" MODIFIED="1572255230114" POSITION="right" TEXT="official website">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/en-us/support/mindstorms-ev3
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572246179060" ID="ID_1916764609" MODIFIED="1572246207550" TEXT="homepage for EV3">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-us/middle-school/intro/mindstorms-ev3">https://education.lego.com/en-us/middle-school/intro/mindstorms-ev3</a>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572246207540" ID="ID_1142215924" MODIFIED="1572246235831" TEXT="download software">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-us/downloads/mindstorms-ev3/software">https://education.lego.com/en-us/downloads/mindstorms-ev3/software</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572247030435" ID="ID_940009455" MODIFIED="1572247122310" TEXT="browser lessons">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-us/lessonsfilter?Products=LEGO%c2%ae+MINDSTORMS%c2%ae+Education+EV3+Core+Set">https://education.lego.com/en-us/lessonsfilter?Products=LEGO%c2%ae+MINDSTORMS%c2%ae+Education+EV3+Core+Set</a>
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572247135252" ID="ID_1102906834" MODIFIED="1572247151730" TEXT="getting started">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-us/support/mindstorms-ev3/getting-started">https://education.lego.com/en-us/support/mindstorms-ev3/getting-started</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572254873235" ID="ID_875849659" MODIFIED="1572254892732" TEXT="CIM">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/en-us/lessons/ev3-cim
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572255367195" ID="ID_1106427332" MODIFIED="1572255413064" TEXT="3rd party support">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/en-us/support/3rd-party-support#LEGO%C2%AE+MINDSTORMS+Education+EV3+compatibility
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572255372531" ID="ID_86249259" MODIFIED="1572255380800" TEXT="swift playgrounds"/>
+</node>
+</node>
+<node CREATED="1572234704334" ID="ID_421865012" MODIFIED="1572242223605" TEXT="Teacher Support">
+<node CREATED="1572235325319" ID="ID_1311275493" MODIFIED="1572235396983" TEXT="overview">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/en-gb/resources/support
+    </p>
+    <p>
+      -- next steps (pdf) baiduyun
+    </p>
+    <p>
+      -- user guild (pdf) baiduyun
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572245799293" ID="ID_148687155" MODIFIED="1572245815823" TEXT=" next steps"/>
+<node CREATED="1572245816863" ID="ID_227875443" MODIFIED="1572245831233" TEXT="user guild"/>
+</node>
+<node CREATED="1572235093950" ID="ID_1602260930" MODIFIED="1572235382665" TEXT="mike it move">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-gb/resources/support/make-it-move">https://education.lego.com/en-gb/resources/support/make-it-move</a>
+    </p>
+    <p>
+      make it move without wheels pdf baiduyun<a href="https://education.lego.com/en-gb/resources/support/make-it-move">
+</a>    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572242225195" ID="ID_1211797923" MODIFIED="1572243098951" TEXT="make it smarter and faster">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-gb/resources/support/make-it-smarter">https://education.lego.com/en-gb/resources/support/make-it-smarter</a>
+    </p>
+    <p>
+      pdf baiduyun
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572243031252" ID="ID_681651566" MODIFIED="1572243100811" TEXT="make a system that communicates">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-gb/resources/support/make-a-system">https://education.lego.com/en-gb/resources/support/make-a-system</a>
+    </p>
+    <p>
+      pdf baiduyun
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1572246035132" ID="ID_433449730" MODIFIED="1572246436009" TEXT="Phython for EV3">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3">https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3 </a>
+    </p>
+    <p>
+      pdf baiduyun
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572245914652" ID="ID_1214982546" MODIFIED="1572245956223" TEXT="help">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://ev3-help-online.api.education.lego.com/Education/en-us/">https://ev3-help-online.api.education.lego.com/Education/en-us/</a>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572245957813" ID="ID_661771514" MODIFIED="1572245960332" TEXT="general"/>
+<node CREATED="1572245999192" ID="ID_540834042" MODIFIED="1572246001432" TEXT="tools"/>
+</node>
+<node CREATED="1572248163146" ID="ID_828952384" MODIFIED="1572248172430" TEXT="training">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://education.lego.com/en-us/training/mindstorms-ev3">https://education.lego.com/en-us/training/mindstorms-ev3</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1572255450483" ID="ID_913074806" MODIFIED="1572255457194" POSITION="left" TEXT="hardware">
+<node CREATED="1572255458267" ID="ID_444903453" MODIFIED="1572256231872" TEXT="LEGO MINDSTORMS Education EV3 Core Set by LEGO Education 5003480">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/en-us/products/lego-mindstorms-education-ev3-core-set-by-lego-education/5003400
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Bricks
+    </p>
+    <p>
+      Includes 541 elements that can be used for teaching science, technology, engineering, math, and computer science.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Curriculum
+    </p>
+    <p>
+      The LEGO MINDSTORMS Education EV3 Core Set comes with a curriculum pack and includes 48 tutorials to help you and your students learn the basics of LEGO MINDSTORMS Education EV3.
+    </p>
+    <p>
+      The 48 step-by-step tutorials are designed to help educators and students master basic and advanced programming as well as hardware and data logging functions.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572255681764" ID="ID_699840880" MODIFIED="1572255683980" TEXT="the EV3 Intelligent Brick"/>
+<node CREATED="1572255738403" ID="ID_1397386927" MODIFIED="1572255759852" TEXT="a storage bin with a sorting tray"/>
+<node CREATED="1572255767187" ID="ID_267063976" MODIFIED="1572255768378" TEXT="three Servo Motors"/>
+<node CREATED="1572255808859" ID="ID_1416376017" MODIFIED="1572255810004" TEXT=" five Sensors (Gyro, Ultrasonic, Color and 2x Touch)"/>
+<node CREATED="1572255841468" ID="ID_1329144060" MODIFIED="1572255842851" TEXT="a EV3 Rechargeable DC Battery (with charging cable) "/>
+<node CREATED="1572255863419" ID="ID_601633492" MODIFIED="1572255865370" TEXT="connecting cables"/>
+</node>
+<node CREATED="1572256107701" ID="ID_1882817014" MODIFIED="1572256227670" TEXT="LEGO MINDSTORMS Education EV3 Expansion Set by LEGO Education 45560">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/en-us/products/lego-mindstorms-education-ev3-expansion-set-by-lego-education/45560
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Bricks
+    </p>
+    <p>
+      Includes 853 bricks and building instructions for 6 showpiece models. Comes complete with a sturdy storage bin with a sorting tray for easy classroom management. Additional building instructions and programs for several models are available.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572317588211" ID="ID_1239236407" MODIFIED="1572317778297" TEXT="history">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://baijiahao.baidu.com/s?id=1608225977182956159&amp;wfr=spider&amp;for=pc
+    </p>
+    <p>
+      1998&#24180;&#65306;LEGO MINDSTORMS&#65288;RCX&#65289;
+    </p>
+    <p>
+      2006&#24180;&#65306;LEGO MINDSTORMS NXT
+    </p>
+    <p>
+      2013&#24180;&#65306;LEGO MINDSTORMS EV3
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1572321398443" ID="ID_1390937586" MODIFIED="1572335578293" TEXT="verification">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      http://www.semia.com/Verification/
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1572318204515" ID="ID_1824494989" MODIFIED="1572318337438" POSITION="right" TEXT="forum">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://bbs.cmnxt.com/
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1572319305715" ID="ID_730656089" MODIFIED="1572319321673" TEXT="others">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://www.chiphell.com/thread-1828095-1-1.html
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1572319704323" ID="ID_292979071" MODIFIED="1572319714416" POSITION="left" TEXT="competitions">
+<node CREATED="1572319716372" ID="ID_1332740437" MODIFIED="1572319734460" TEXT="fll">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://education.lego.com/zh-cn/competitions
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      http://www.firstlegoleague.org/
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      http://robot.xiaoxiaotong.org/2019/
+    </p>
+    <p>
+      http://robot.xiaoxiaotong.org/AttachFile/2018/11/1013000100/636772122226779776.pdf
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
 </map>
